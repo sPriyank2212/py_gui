79fc3e61873a65fb75cadf648943d333afbc125c
Priyank Soni <spriyank683@gmail.com>
2024-05-18 13:40:54 +0530

    first commit

---
+++ #HT_04.err.tcl
@@ -0,0 +1,16 @@
+can't create directory "./backups": permission denied
+    while executing
+"file mkdir $bk_dir"
+    (procedure "vTcl:backup_files" line 8)
+    invoked from within
+"vTcl:backup_files $file"
+    (procedure "vTcl:save2" line 180)
+    invoked from within
+"vTcl:save2 $vTcl(project,file)"
+    (procedure "vTcl:save" line 10)
+    invoked from within
+"vTcl:save"
+    (procedure "vTcl:generate_python_UI" line 11)
+    invoked from within
+"vTcl:generate_python_UI"
+    (menu invoke)
